4c3f23dc1b5947277026701ea623e9acf914c0cd
alex <melnikov1991@bk.ru>
2020-09-14 17:38:47 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private IProductRepository repository;
+
+
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+
+        }
+
+        public ViewResult List() => View(repository.Products);
+
+
+    }
+}
